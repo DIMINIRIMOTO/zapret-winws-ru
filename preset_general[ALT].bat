7d0002e6aa72cf8c1a415be690a6ad95dc486449
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2025-04-11 09:17:08 +0700

    Delete preset_general[ALT].bat

--- preset_general[ALT].bat
+++
@@ -1,9 +0,0 @@
-cd /d "%~dp0"
-set BIN=%~dp0bins\
-
-start "zapret: http,https,quic" /min "%BIN%winws.exe" ^
---wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-udp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
---filter-udp=50000-50099 --ipset="%~dp0files\ipset-discord.txt" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
---filter-tcp=80 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
---filter-tcp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,rndsni,padencap

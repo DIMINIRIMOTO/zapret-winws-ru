6af63bab45dfbfc6f4ba034d70d4985a48441d68
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2025-11-07 17:24:56 +0700

    Исправление ошибки
    Исправление ошибки

--- service_create[ALT].bat
+++ service_create[ALT].bat
@@ -9,7 +9,7 @@ chcp 1251 > nul
 cd /d "%~dp0"
 set BIN_PATH=%~dp0bins\
 set ARGS=^
---wf-tcp=80,443,444-65535 --wf-udp=443,444-65535,50000-50099 ^
+--wf-tcp=80,443 --wf-udp=443,50000-50099,444-65535 ^
 --filter-udp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
 --filter-tcp=80 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=split2 --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_www_google_com.bin" --new ^
@@ -22,3 +22,4 @@ sc create "zapret" binPath= "\"%BIN_PATH%winws.exe\" %ARGS%" DisplayName= "zapre
 sc description "zapret" "zapret DPI bypass software"
 sc start "zapret"
 pause
+

bb6611b1b0f2dfded60d1fdaa9c860412faa8d8b
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2025-11-10 10:04:40 +0700

    БАЗА
    ОСНОВА

--- service_create.cmd
+++ service_create.cmd
@@ -10,11 +10,12 @@ cd /d "%~dp0"
 set BIN_PATH=%~dp0bins\
 set ARGS=^
 --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-udp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
 --filter-tcp=80 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake,multisplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_www_google_com.bin"
 sc create "zapret" binPath= "\"%BIN_PATH%winws.exe\" %ARGS%" DisplayName= "zapret DPI bypass : winws1" start= auto
 sc description "zapret" "zapret DPI bypass software"
 sc start "zapret"
+
 pause

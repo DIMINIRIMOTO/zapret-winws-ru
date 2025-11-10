09fb2eda10572ccc175289d254c09acf14abf264
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2025-11-10 10:01:35 +0700

    БАЗА
    ОСНОВА

---
+++ preset_general[ALT].bat
@@ -0,0 +1,12 @@
+cd /d "%~dp0"
+set BIN=%~dp0bins\
+start "zapret: http,https,quic" /min "%BIN%winws.exe" ^
+--wf-tcp=80,443,12 --wf-udp=443,50000-50099,12 ^
+--filter-udp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
+--filter-tcp=80 --hostlist="%~dp0files\list-general.txt" --dpi-desync=fake,multisplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%~dp0files\list-general.txt" --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_www_google_com.bin" --new ^
+--filter-udp=443 --ipset="%~dp0files\ipset-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --ipset="%~dp0files\ipset-general.txt" --dpi-desync=fake,multisplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443,12 --ipset=""%~dp0files\ipset-general.txt" --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_www_google_com.bin" --new ^
+--filter-udp=12 --ipset="%~dp0files\ipset-general.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=12 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-cutoff=n2
---
+++ service_remove.cmd
@@ -0,0 +1,5 @@
+@echo off
+set SRVCNAME=zapret
+net stop "%SRVCNAME%"
+sc delete "%SRVCNAME%"
+pause

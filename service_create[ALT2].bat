848ab54527046281b9f699500b096c86ca798cab
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2025-12-27 11:01:30 +0700

    WINWS 72.2
    NEW PRESETS/NEW UNLOCK RU (GAME NOT TESTED)

---
+++ service_create[ALT2].bat
@@ -0,0 +1,27 @@
+@echo off
+pushd "%~dp0"
+sc query zapret >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    sc stop "zapret"
+    sc delete "zapret"
+)
+chcp 65001 > nul
+cd /d "%~dp0"
+call universal.bat load_game_filter
+echo:
+set BIN_PATH=%~dp0bins\
+set ARGS=^
+--wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilter% --wf-udp=443,19294-19344,50000-50099,%GameFilter% ^
+--filter-udp=443 --hostlist="%~dp0files\list-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-udp=19294-19344,50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
+--filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=654 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_max_ru.bin" --dpi-desync-fake-tls="%~dp0files\tls_clienthello_max_ru.bin" --new ^
+--filter-tcp=443 --hostlist="%~dp0files\list-youtube.txt" --ip-id=zero --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_www_google_com.bin" --dpi-desync-fake-tls="%~dp0files\tls_clienthello_www_google_com.bin" --new ^
+--filter-tcp=80,443 --hostlist="%~dp0files\list-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=654 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_max_ru.bin" --dpi-desync-fake-tls="%~dp0files\tls_clienthello_max_ru.bin" --new ^
+--filter-udp=443 --ipset="%~dp0files\ipset-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80,443,%GameFilter% --ipset="%~dp0files\ipset-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=654 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern="%~dp0files\tls_clienthello_max_ru.bin" --dpi-desync-fake-tls="%~dp0files\tls_clienthello_max_ru.bin" --new ^
+--filter-udp=%GameFilter% --ipset="%~dp0files\ipset-general.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-cutoff=n2
+sc create "zapret" binPath= "\"%BIN_PATH%winws.exe\" %ARGS%" DisplayName= "zapret DPI bypass : winws1" start= auto
+sc description "zapret" "zapret DPI bypass software"
+sc start "zapret"
+pause
+exit /b

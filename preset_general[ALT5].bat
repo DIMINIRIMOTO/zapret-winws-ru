f0e66e5c0c5be64867cd981e01498421cdef736a
Kelse <57309845+DIMINIRIMOTO@users.noreply.github.com>
2026-04-30 08:24:13 +0700

    Update preset_general[ALT5].bat

--- preset_general[ALT5].bat
+++ preset_general[ALT5].bat
@@ -27,7 +27,7 @@ set BIN_PATH=%~dp0bins\
 set ARGS=^
 --wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilterTCP% --wf-udp=443,19294-19344,50000-50100,%GameFilterUDP% ^
 --filter-udp=443 --hostlist="%~dp0files\list-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
---filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord="%~dp0files\quic_initial_dbankcloud_ru.bin" --dpi-desync-fake-stun="%~dp0files\quic_initial_dbankcloud_ru.bin" --dpi-desync-repeats=6 --new ^
+--filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-fake-stun="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-repeats=6 --new ^
 --filter-l3=ipv4 --filter-tcp=80,443,2053,2083,2087,2096,8443 --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=syndata,multidisorder --new ^
 --filter-tcp=%GameFilterTCP% --ipset="%~dp0files\ipset-general.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=syndata,multidisorder --dpi-desync-any-protocol=1 --dpi-desync-cutoff=n4 --new ^
 --filter-udp=443 --ipset="%~dp0files\ipset-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
@@ -45,7 +45,7 @@ set BIN=%~dp0bins\
 start "zapret: http,https,quic" /min "%BIN%winws.exe" ^
 --wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilterTCP% --wf-udp=443,19294-19344,50000-50100,%GameFilterUDP% ^
 --filter-udp=443 --hostlist="%~dp0files\list-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
---filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord="%~dp0files\quic_initial_dbankcloud_ru.bin" --dpi-desync-fake-stun="%~dp0files\quic_initial_dbankcloud_ru.bin" --dpi-desync-repeats=6 --new ^
+--filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-fake-stun="%~dp0files\quic_initial_www_google_com.bin" --dpi-desync-repeats=6 --new ^
 --filter-l3=ipv4 --filter-tcp=80,443,2053,2083,2087,2096,8443 --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=syndata,multidisorder --new ^
 --filter-tcp=%GameFilterTCP% --ipset="%~dp0files\ipset-general.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=syndata,multidisorder --dpi-desync-any-protocol=1 --dpi-desync-cutoff=n4 --new ^
 --filter-udp=443 --ipset="%~dp0files\ipset-general.txt" --hostlist-exclude="%~dp0files\list-exclude.txt" --ipset-exclude="%~dp0files\ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
